c9daacba1f522e2f6c320b63e14d69f3449a9c16
Abdul Rafey <syedabdulrafeyali@gmail.com>
2024-12-08 00:18:55 +0100

    Project Setup + Installation

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return (
+        <div className='sticky top-0 bg-white z-10'>
+            <div className="continuer hidden lg:block">
+                <div className="flex justify-between items-center p-8">
+                    <h1 className="text-4xl font-medium">Logo</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
